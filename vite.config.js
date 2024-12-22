0b29e87827e3d3c3ac0485566b169288c3d048c6
salah <cloud.data.sa@gmail.com>
2024-12-22 03:19:05 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
+            // ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
+        react(),
     ],
 });
